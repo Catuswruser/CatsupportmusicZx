58362c30812bddc261dd22a5f8d3b2229e4b47c0
Catuswruser <167173659+Catuswruser@users.noreply.github.com>
2024-04-16 13:33:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("SEX")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
